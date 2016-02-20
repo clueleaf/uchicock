691897abe6c108b858b8d68e5a13c7c5ba18af13
Kou <kou.kinyo@gmail.com>
2016-02-20 17:48:38 +0900

    cellのレイアウトを設定

---
+++ RecipeListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeListItemTableViewCell.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2016/02/20.
+//  Copyright © 2016年 Kou. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
