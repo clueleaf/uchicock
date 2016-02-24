608862297bbe7abbd98e65c4078fb326da3c2b47
Kou <kou.kinyo@gmail.com>
2016-02-24 20:45:24 +0900

    add recipe database and sample datas

---
+++ uchicock/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2016/02/24.
+//  Copyright © 2016年 Kou. All rights reserved.
+//
+
+//import Foundation
+import RealmSwift
+
+class Recipe: Object {
+    dynamic var recipeName = ""
+}
--- uchicock/RecipeListViewController.swift
+++ uchicock/RecipeListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class RecipeListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
@@ -15,12 +16,77 @@ class RecipeListViewController: UIViewController, UITableViewDelegate, UITableVi
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.AddData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - RealmSettings
+    
+    func AddData() {
+        
+        let recipe = Recipe()
+        recipe.recipeName = "テキーラサンライズ"
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(recipe)
+        }
+
+        //        self.save()
+        //        self.dataUpdate()
+//        self.dataDelete()
+        
+//        self.dataGet()
+    }
+    
+    //データの保存
+//    func save() {
+//        do {
+//            let realm = try Realm()
+//            realm.write {
+//                realm.add(self.recipe)
+//            }
+//        } catch {
+//            // Error handling...
+//        }
+//    }
+    
+    // データの取得
+//    func dataGet() {
+//        let realm = try! Realm()
+        
+//        let dataContent = realm.objects(Recipe)
+//        print(dataContent)
+//    }
+    
+    // データの更新
+//    func dataUpdate() {
+//        let realm = try! Realm()
+        
+//        let user = realm.objects(Recipe).last!
+//        realm.write {
+//            user.recipeName = "Yamasaki Tarou"
+//        }
+//    }
+    
+    // データの削除
+//    func dataDelete() {
+//        let realm = try! Realm()
+        
+//        let user = realm.objects(Recipe).last!
+//        realm.write {
+            // 最後のデータ
+//            realm.delete(user)
+            // 全てのデータ
+            //          realm.deleteAll()
+//        }
+//    }
+    
     
     // MARK: - UITableViewDelegate
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -33,9 +99,15 @@ class RecipeListViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let realm = try! Realm()
+        let dataContent = realm.objects(Recipe)
+        
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCellWithIdentifier("RecipeListItem") as! RecipeListItemTableViewCell
-            cell.recipeName.text = "\(indexPath.row)"
+            cell.recipeName.text = dataContent[0].recipeName
+            cell.shortage.text = "\(indexPath.row)"
+            cell.favorites.text = "★★☆"
             return cell
         }
         return UITableViewCell()
