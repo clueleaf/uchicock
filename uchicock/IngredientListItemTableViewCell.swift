d9422d12b1d53e8ee872d794300b7d635a852b6e
Kou <kou.kinyo@gmail.com>
2016-03-18 22:45:17 +0900

    Tab Bar Controllerを設定

---
+++ uchicock/IngredientListItemTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  IngredientListItemTableViewCell.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2016/03/18.
+//  Copyright © 2016年 Kou. All rights reserved.
+//
+
+import UIKit
+
+class IngredientListItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ingredientName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ uchicock/IngredientListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  IngredientListViewController.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2016/03/18.
+//  Copyright © 2016年 Kou. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class IngredientListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("IngredientListItem") as! IngredientListItemTableViewCell
+            cell.ingredientName.text = "\(indexPath.row)"
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
