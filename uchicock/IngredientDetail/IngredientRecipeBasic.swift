93ebed7de6647f68b7b30c6c6ab60832571bb0f0
Kou <kou.kinyo@gmail.com>
2016-04-11 21:34:01 +0900

    材料詳細画面のレシピ一覧の表示順を名前順に変更

---
+++ uchicock/IngredientDetail/IngredientRecipeBasic.swift
@@ -0,0 +1,15 @@
+//
+//  IngredientRecipeListBasic.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2016/04/11.
+//  Copyright © 2016年 Kou. All rights reserved.
+//
+
+import UIKit
+
+class IngredientRecipeBasic: NSObject {
+    var recipeIngredientLinkId = ""
+    var recipeName = ""
+}
+
