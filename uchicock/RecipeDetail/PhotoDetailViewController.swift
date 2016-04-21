fe49e0980c15790ea186ab43342068f64c94eba3
Kou <kou.kinyo@gmail.com>
2016-04-21 19:05:10 +0900

    カメラロールへ保存が成功した際のメッセージを自動で閉じる

--- uchicock/RecipeDetail/PhotoDetailViewController.swift
+++ uchicock/RecipeDetail/PhotoDetailViewController.swift
@@ -44,9 +44,13 @@ class PhotoDetailViewController: UIViewController, UIScrollViewDelegate {
     func image(image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutablePointer<Void>) {
         if error == nil{
             let alertView = UIAlertController(title: "カメラロールへ保存しました", message: nil, preferredStyle: .Alert)
-            alertView.addAction(UIAlertAction(title: "OK", style: .Default, handler: {action in
-            }))
-            presentViewController(alertView, animated: true, completion: nil)
+            self.presentViewController(alertView, animated: true) { () -> Void in
+                let delay = 1.0 * Double(NSEC_PER_SEC)
+                let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+                dispatch_after(time, dispatch_get_main_queue(), {
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                })
+            }
         }else{
             let alertView = UIAlertController(title: "カメラロールへの保存に失敗しました", message: "「設定」→「うちカク！」にて写真へのアクセス許可を確認してください", preferredStyle: .Alert)
             alertView.addAction(UIAlertAction(title: "OK", style: .Default, handler: {action in
