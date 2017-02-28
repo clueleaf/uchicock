034f85c380edc9b6e8ada984b80471bd4bc109e9
Kou <kou.kinyo@gmail.com>
2017-02-28 21:07:41 +0900

    テーマ変更後にnavigationBarが色崩れしないようにnavigationControllerを追加

---
+++ uchicock/NavigationControllers/RecipeNavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeNavigationController.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2017/02/28.
+//  Copyright © 2017年 Kou. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class RecipeNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationBar.tintColor = ContrastColorOf(Style.primaryColor, returnFlat: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }    
+}
---
+++ uchicock/NavigationControllers/SettingsNavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsNavigationController.swift
+//  uchicock
+//
+//  Created by Kou Kinyo on 2017/02/28.
+//  Copyright © 2017年 Kou. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class SettingsNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationBar.tintColor = ContrastColorOf(Style.primaryColor, returnFlat: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
