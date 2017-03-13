e671ef3b728f2fc7ea93f5cd9df13f1d6684d954
Kou <kou.kinyo@gmail.com>
2017-03-13 20:55:50 +0900

    逆引き結果のtableViewを追加

--- uchicock/ReverseLookup/ReverseLookupViewController.swift
+++ uchicock/ReverseLookup/ReverseLookupViewController.swift
@@ -14,26 +14,37 @@ import DZNEmptyDataSet
 class ReverseLookupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var ingredientTableView: UITableView!
+    @IBOutlet weak var recipeTableView: UITableView!
+
     var firstIngredientLabel = ""
     var secondIngredientLabel = ""
     var thirdIngredientLabel = ""
-    
+    var recipeList: Results<Recipe>?
+    var recipeBasicList = Array<RecipeBasic>()
+    let selectedCellBackgroundView = UIView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ingredientTableView.tag = 0
+        recipeTableView.tag = 1
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let selectedPathForIngredientTableView = ingredientTableView.indexPathForSelectedRow
+        // recipeTableViewをdeselectする
         
+        selectedCellBackgroundView.backgroundColor = Style.tableViewCellSelectedBackgroundColor
+
         // Userdefaultsからロードする
         firstIngredientLabel = ""
         secondIngredientLabel = ""
         thirdIngredientLabel = ""
 
         ingredientTableView.reloadData()
+        reloadRecipeList()
+        recipeTableView.reloadData()
 
         if let path = selectedPathForIngredientTableView{
             ingredientTableView.selectRow(at: path, animated: false, scrollPosition: .none)
@@ -41,10 +52,35 @@ class ReverseLookupViewController: UIViewController, UITableViewDelegate, UITabl
                 self.ingredientTableView.deselectRow(at: path, animated: true)
             }
         }
+        // recipeTableViewをdeselectする
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func reloadRecipeList(){
+        let realm = try! Realm()
+        recipeList = realm.objects(Recipe.self).sorted(byKeyPath: "recipeName")
+        try! realm.write {
+            for recipe in recipeList!{
+                recipe.updateShortageNum()
+            }
+        }
+        reloadRecipeBasicList()
+    }
+    
+    func reloadRecipeBasicList(){
+        recipeBasicList.removeAll()
+        for recipe in recipeList!{
+            recipeBasicList.append(RecipeBasic(id: recipe.id, name: recipe.recipeName, shortageNum: recipe.shortageNum, favorites: recipe.favorites, japaneseDictionaryOrder: recipe.japaneseDictionaryOrder))
+        }
+        
+        // 対象外のレシピを除外する
+        
+        recipeBasicList.sort(by: { $0.japaneseDictionaryOrder < $1.japaneseDictionaryOrder })
+        
+        // ヘッダーのレシピ数を更新する
     }
     
     // MARK: - UITableView
@@ -52,7 +88,7 @@ class ReverseLookupViewController: UIViewController, UITableViewDelegate, UITabl
         if tableView.tag == 0{
             return 4
         }else if tableView.tag == 1{
-            return 0
+            return recipeBasicList.count
         }
         return 0
     }
@@ -68,7 +104,7 @@ class ReverseLookupViewController: UIViewController, UITableViewDelegate, UITabl
                 return 0
             }
         }else if tableView.tag == 1{
-            return 0
+            return 70
         }
         return 0
     }
@@ -122,7 +158,74 @@ class ReverseLookupViewController: UIViewController, UITableViewDelegate, UITabl
                 return cell
             }
         }else if tableView.tag == 1{
-            return UITableViewCell()
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeList", for: indexPath) as! IngredientRecipeListTableViewCell
+            
+            let realm = try! Realm()
+            let recipe = realm.objects(Recipe.self).filter("id == %@",recipeBasicList[indexPath.row ].id).first!
+            
+            if let image = recipe.imageData {
+                cell.photo.image = UIImage(data: image as Data)
+                //レシピ削除のバグに対するワークアラウンド
+                if cell.photo.image == nil{
+                    if Style.isDark{
+                        cell.photo.image = UIImage(named: "no-photo-dark")
+                    }else{
+                        cell.photo.image = UIImage(named: "no-photo")
+                    }
+                }
+            }else{
+                if Style.isDark{
+                    cell.photo.image = UIImage(named: "no-photo-dark")
+                }else{
+                    cell.photo.image = UIImage(named: "no-photo")
+                }
+            }
+            
+            cell.recipeId = recipe.id
+            cell.recipeName.text = recipe.recipeName
+            switch recipe.favorites{
+            case 1:
+                cell.favorites.text = "★　　"
+            case 2:
+                cell.favorites.text = "★★　"
+            case 3:
+                cell.favorites.text = "★★★"
+            default:
+                cell.favorites.text = "★　　"
+            }
+            cell.favorites.textAlignment = .left
+            cell.favorites.textColor = Style.secondaryColor
+            
+            var shortageNum = 0
+            var shortageName = ""
+            for ri in recipe.recipeIngredients{
+                if ri.mustFlag && ri.ingredient.stockFlag == false{
+                    shortageNum += 1
+                    shortageName = ri.ingredient.ingredientName
+                }
+            }
+            if shortageNum == 0 {
+                cell.shortage.text = "すぐつくれる！"
+                cell.shortage.textColor = Style.secondaryColor
+                cell.shortage.font = UIFont.boldSystemFont(ofSize: CGFloat(14))
+                cell.recipeName.textColor = Style.labelTextColor
+            }else if shortageNum == 1{
+                cell.shortage.text = shortageName + "が足りません"
+                cell.shortage.textColor = Style.labelTextColorLight
+                cell.shortage.font = UIFont.systemFont(ofSize: CGFloat(14))
+                cell.recipeName.textColor = Style.labelTextColorLight
+            }else{
+                cell.shortage.text = "材料が" + String(shortageNum) + "個足りません"
+                cell.shortage.textColor = Style.labelTextColorLight
+                cell.shortage.font = UIFont.systemFont(ofSize: CGFloat(14))
+                cell.recipeName.textColor = Style.labelTextColorLight
+            }
+            
+            cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+            cell.selectionStyle = .default
+            cell.backgroundColor = Style.basicBackgroundColor
+            cell.selectedBackgroundView = selectedCellBackgroundView
+            return cell
         }
         return UITableViewCell()
     }
