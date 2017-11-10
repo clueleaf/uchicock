3d3ec5e5f9b6d3e4a2a483e7b16ff39e31513dc2
Kou <kou.kinyo@gmail.com>
2017-11-10 23:25:35 +0900

    SVProgressHUDの最小サイズを設定

--- uchicock/AppDelegate.swift
+++ uchicock/AppDelegate.swift
@@ -211,6 +211,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func setSVProgressHUD(){
         SVProgressHUD.setMinimumDismissTimeInterval(2.0)
         SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.dark)
+        SVProgressHUD.setMinimumSize(CGSize(width: 150, height: 100))
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
