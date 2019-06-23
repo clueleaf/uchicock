73ba5be7b1025d41d025520585eee8eef190a52a
Kou <kou.kinyo@gmail.com>
2019-06-23 17:30:08 +0900

    テーマカラーにコメントをつける

--- uchicock/Settings/FlatColor.swift
+++ uchicock/Settings/FlatColor.swift
@@ -9,51 +9,116 @@
 struct FlatColor{
     static let black = UIColor(hue: 0/360, saturation: 0/100, brightness: 17/100, alpha: 1)
     static let blackDark = UIColor(hue: 0/360, saturation: 0/100, brightness: 15/100, alpha: 1)
-    static let maroon = UIColor(hue: 5/360, saturation: 65/100, brightness: 47/100, alpha: 1)
-    static let maroonDark = UIColor(hue: 4/360, saturation: 68/100, brightness: 40/100, alpha: 1)
-    static let red = UIColor(hue: 6/360, saturation: 74/100, brightness: 91/100, alpha: 1)
-    static let redDark = UIColor(hue: 6/360, saturation: 78/100, brightness: 75/100, alpha: 1)
-    static let brown = UIColor(hue: 24/360, saturation: 45/100, brightness: 37/100, alpha: 1)
-    static let brownDark = UIColor(hue: 25/360, saturation: 45/100, brightness: 31/100, alpha: 1)
-    static let coffee = UIColor(hue: 25/360, saturation: 31/100, brightness: 64/100, alpha: 1)
-    static let coffeeDark = UIColor(hue: 25/360, saturation: 34/100, brightness: 56/100, alpha: 1)
-    static let orange = UIColor(hue: 28/360, saturation: 85/100, brightness: 90/100, alpha: 1)
-    static let orangeDark = UIColor(hue: 24/360, saturation: 100/100, brightness: 83/100, alpha: 1)
-    static let sand = UIColor(hue: 42/360, saturation: 25/100, brightness: 94/100, alpha: 1)
-    static let sandDark = UIColor(hue: 42/360, saturation: 30/100, brightness: 84/100, alpha: 1)
-    static let yellow = UIColor(hue: 48/360, saturation: 99/100, brightness: 100/100, alpha: 1)
-    static let yellowDark = UIColor(hue: 40/360, saturation: 100/100, brightness: 100/100, alpha: 1)
-    static let lime = UIColor(hue: 74/360, saturation: 70/100, brightness: 78/100, alpha: 1)
-    static let limeDark = UIColor(hue: 74/360, saturation: 81/100, brightness: 69/100, alpha: 1)
-    static let forestGreen = UIColor(hue: 138/360, saturation: 45/100, brightness: 37/100, alpha: 1)
-    static let forestGreenDark = UIColor(hue: 135/360, saturation: 44/100, brightness: 31/100, alpha: 1)
-    static let green = UIColor(hue: 145/360, saturation: 77/100, brightness: 80/100, alpha: 1)
-    static let greenDark = UIColor(hue: 145/360, saturation: 78/100, brightness: 68/100, alpha: 1)
-    static let mint = UIColor(hue: 168/360, saturation: 86/100, brightness: 74/100, alpha: 1)
-    static let mintDark = UIColor(hue: 168/360, saturation: 86/100, brightness: 63/100, alpha: 1)
+    
     static let gray = UIColor(hue: 184/360, saturation: 10/100, brightness: 65/100, alpha: 1)
     static let grayDark = UIColor(hue: 184/360, saturation: 10/100, brightness: 55/100, alpha: 1)
+    
     static let white = UIColor(hue: 192/360, saturation: 2/100, brightness: 95/100, alpha: 1)
     static let whiteDark = UIColor(hue: 204/360, saturation: 5/100, brightness: 78/100, alpha: 1)
+    
+    //（暗い）
+    static let maroon = UIColor(hue: 5/360, saturation: 65/100, brightness: 47/100, alpha: 1)
+    //（暗い）
+    static let maroonDark = UIColor(hue: 4/360, saturation: 68/100, brightness: 40/100, alpha: 1)
+
+    // Delete Colorとして利用
+    static let red = UIColor(hue: 6/360, saturation: 74/100, brightness: 91/100, alpha: 1)
+    // Delete Colorとして利用
+    static let redDark = UIColor(hue: 6/360, saturation: 78/100, brightness: 75/100, alpha: 1)
+
+    //（暗い）
+    static let brown = UIColor(hue: 24/360, saturation: 45/100, brightness: 37/100, alpha: 1)
+    //（暗い）
+    static let brownDark = UIColor(hue: 25/360, saturation: 45/100, brightness: 31/100, alpha: 1)
+
+    // ブラックルシアン、アイリッシュコーヒー
+    static let coffee = UIColor(hue: 25/360, saturation: 31/100, brightness: 64/100, alpha: 1)
+    // ブラックルシアン、アイリッシュコーヒー
+    static let coffeeDark = UIColor(hue: 25/360, saturation: 34/100, brightness: 56/100, alpha: 1)
+
+    // ミモザ、テキーラサンライズ（一部）
+    static let orange = UIColor(hue: 28/360, saturation: 85/100, brightness: 90/100, alpha: 1)
+    // ミモザ
+    static let orangeDark = UIColor(hue: 24/360, saturation: 100/100, brightness: 83/100, alpha: 1)
+
+    // テキーラサンライズ（一部）
+    static let sand = UIColor(hue: 42/360, saturation: 25/100, brightness: 94/100, alpha: 1)
+    // ブラックルシアン（一部）
+    static let sandDark = UIColor(hue: 42/360, saturation: 30/100, brightness: 84/100, alpha: 1)
+
+    // テキーラサンライズ
+    static let yellow = UIColor(hue: 48/360, saturation: 99/100, brightness: 100/100, alpha: 1)
+    // テキーラサンライズ、ミモザ（一部）
+    static let yellowDark = UIColor(hue: 40/360, saturation: 100/100, brightness: 100/100, alpha: 1)
+
+    //
+    static let lime = UIColor(hue: 74/360, saturation: 70/100, brightness: 78/100, alpha: 1)
+    // キューバリバー
+    static let limeDark = UIColor(hue: 74/360, saturation: 81/100, brightness: 69/100, alpha: 1)
+
+    //（暗い）
+    static let forestGreen = UIColor(hue: 138/360, saturation: 45/100, brightness: 37/100, alpha: 1)
+    //（暗い）
+    static let forestGreenDark = UIColor(hue: 135/360, saturation: 44/100, brightness: 31/100, alpha: 1)
+
+    //
+    static let green = UIColor(hue: 145/360, saturation: 77/100, brightness: 80/100, alpha: 1)
+    // 照葉樹林
+    static let greenDark = UIColor(hue: 145/360, saturation: 78/100, brightness: 68/100, alpha: 1)
+
+    // グラスホッパー、モヒート
+    static let mint = UIColor(hue: 168/360, saturation: 86/100, brightness: 74/100, alpha: 1)
+    // グラスホッパー
+    static let mintDark = UIColor(hue: 168/360, saturation: 86/100, brightness: 63/100, alpha: 1)
+
+    //（無機質）
     static let teal = UIColor(hue: 195/360, saturation: 55/100, brightness: 51/100, alpha: 1)
+    //（無機質）
     static let tealDark = UIColor(hue: 196/360, saturation: 54/100, brightness: 45/100, alpha: 1)
+
+    // チャイナブルー
     static let skyBlue = UIColor(hue: 204/360, saturation: 76/100, brightness: 86/100, alpha: 1)
+    // チャイナブルー、ブルーラグーン、テキーラサンライズ（一部）
     static let skyBlueDark = UIColor(hue: 204/360, saturation: 78/100, brightness: 73/100, alpha: 1)
+
+    //（暗い）
     static let navyBlue = UIColor(hue: 210/360, saturation: 45/100, brightness: 37/100, alpha: 1)
+    //（暗い）
     static let navyBlueDark = UIColor(hue: 210/360, saturation: 45/100, brightness: 31/100, alpha: 1)
+
+    //
     static let powderBlue = UIColor(hue: 222/360, saturation: 24/100, brightness: 95/100, alpha: 1)
+    //（無機質）
     static let powderBlueDark = UIColor(hue: 222/360, saturation: 28/100, brightness: 84/100, alpha: 1)
+
+    //
     static let blue = UIColor(hue: 224/360, saturation: 50/100, brightness: 63/100, alpha: 1)
+    //（暗い）
     static let blueDark = UIColor(hue: 224/360, saturation: 56/100, brightness: 51/100, alpha: 1)
+
+    //
     static let purple = UIColor(hue: 253/360, saturation: 52/100, brightness: 77/100, alpha: 1)
+    //
     static let purpleDark = UIColor(hue: 253/360, saturation: 56/100, brightness: 64/100, alpha: 1)
+
+    //
     static let magenta = UIColor(hue: 283/360, saturation: 51/100, brightness: 71/100, alpha: 1)
+    //
     static let magentaDark = UIColor(hue: 282/360, saturation: 61/100, brightness: 68/100, alpha: 1)
+
+    //（暗い）
     static let plum = UIColor(hue: 300/360, saturation: 45/100, brightness: 37/100, alpha: 1)
+    //（暗い）
     static let plumDark = UIColor(hue: 300/360, saturation: 46/100, brightness: 31/100, alpha: 1)
+
+    // シーブリーズ、ピンクレディ
     static let pink = UIColor(hue: 324/360, saturation: 49/100, brightness: 96/100, alpha: 1)
+    // シーブリーズ、ピンクレディ
     static let pinkDark = UIColor(hue: 327/360, saturation: 57/100, brightness: 83/100, alpha: 1)
+
+    // Delete color, レッドアイ
     static let watermelon = UIColor(hue: 356/360, saturation: 53/100, brightness: 94/100, alpha: 1)
+    // Delete color, レッドアイ、アメリカンレモネード
     static let watermelonDark = UIColor(hue: 358/360, saturation: 61/100, brightness: 85/100, alpha: 1)
     
     static func contrastColorOf(_ primeColor: UIColor, isFlat: Bool) -> UIColor{
