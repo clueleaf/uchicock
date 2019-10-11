e6104c2c00911cadbe0610dba7fb7341a8d56288
Kou <kou.kinyo@gmail.com>
2019-10-12 02:01:19 +0900

    逆引き画面表示時には常にレシピテーブルを初期表示する

--- uchicock/ReverseLookup/ReverseLookupTableViewController.swift
+++ uchicock/ReverseLookup/ReverseLookupTableViewController.swift
@@ -92,6 +92,10 @@ class ReverseLookupTableViewController: UITableViewController, UITextFieldDelega
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupVC()
+
+        // iPadや画面回転でキーボードが消えるため、他のタブに行ってデータの更新が可能
+        // 整合性のために逆引き表示では常にレシピテーブルを表示するようにする
+        showRecipeTableView()
     }
     
     private func setupVC(){
