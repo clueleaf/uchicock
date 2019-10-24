7275c6d5e2414c9e66f4d29e7c75919b725fef92
Kou <kou.kinyo@gmail.com>
2019-10-24 18:47:10 +0900

    DBスキーマバージョン更新

--- uchicock/Util/GlobalConstants.swift
+++ uchicock/Util/GlobalConstants.swift
@@ -17,7 +17,7 @@ struct GlobalConstants{
     static let ThumbnailFolderPath = GlobalConstants.DocumentDir.appendingPathComponent(GlobalConstants.RecipeThumbnailsDirectory)
 
     // MARK: - Database Version
-    static let RealmSchemaVersion: UInt64 = 9
+    static let RealmSchemaVersion: UInt64 = 10
     
     // MARK: - Launch Management Keys
     static let FirstLaunchKey = "firstLaunch"
