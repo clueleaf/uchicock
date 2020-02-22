8672d0abd6c52a10ac5a6eb1bba07291d853326e
Kou <kou.kinyo@gmail.com>
2020-02-21 22:46:48 -0500

    366種類目のサンプルレシピを追加

--- uchicock/Util/ImageUtil.swift
+++ uchicock/Util/ImageUtil.swift
@@ -103,6 +103,6 @@ final class ImageCache: NSCache<NSString, UIImage> {
     static let shared = ImageCache()
     private override init() {
         super.init()
-        self.countLimit = 365
+        self.countLimit = 366
     }
 }
