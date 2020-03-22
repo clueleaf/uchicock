db12a7b0dd31b55d4b94871af47f130613c39f77
Kou <kou.kinyo@gmail.com>
2020-03-22 19:57:15 -0400

    TodayExtensionを追加

---
+++ uchicock Widget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  uchicock Widget
+//
+//  Created by Kou Kinyo on 2020-03-22.
+//  Copyright © 2020 Kou. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
