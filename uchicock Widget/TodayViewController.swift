0df3db983456ad614276ffe922ff0ea4d0031a06
Kou <kou.kinyo@gmail.com>
2020-03-23 08:20:02 -0400

    iOS 12もToday Extensionに対応

--- uchicock Widget/TodayViewController.swift
+++ uchicock Widget/TodayViewController.swift
@@ -29,23 +29,51 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
         bookmarkButton.layer.cornerRadius = bookmarkButton.frame.size.width / 2
         bookmarkButton.clipsToBounds = true
-        bookmarkButton.backgroundColor = UIColor(named: "buttonBackground")
-        bookmarkButton.tintColor = UIColor.label
+        if #available(iOSApplicationExtension 13.0, *) {
+            bookmarkButton.backgroundColor = UIColor(named: "buttonBackground")
+            bookmarkButton.tintColor = UIColor.label
+            bookmarkLabel.textColor = UIColor.label
+        }else{
+            bookmarkButton.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+            bookmarkButton.tintColor = UIColor.black
+            bookmarkLabel.textColor = UIColor.black
+        }
         
         reminderButton.layer.cornerRadius = reminderButton.frame.size.width / 2
         reminderButton.clipsToBounds = true
-        reminderButton.backgroundColor = UIColor(named: "buttonBackground")
-        reminderButton.tintColor = UIColor.label
-        
+        if #available(iOSApplicationExtension 13.0, *) {
+            reminderButton.backgroundColor = UIColor(named: "buttonBackground")
+            reminderButton.tintColor = UIColor.label
+            reminderLabel.textColor = UIColor.label
+        }else{
+            reminderButton.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+            reminderButton.tintColor = UIColor.black
+            reminderLabel.textColor = UIColor.black
+        }
+
         albumButton.layer.cornerRadius = albumButton.frame.size.width / 2
         albumButton.clipsToBounds = true
-        albumButton.backgroundColor = UIColor(named: "buttonBackground")
-        albumButton.tintColor = UIColor.label
+        if #available(iOSApplicationExtension 13.0, *) {
+            albumButton.backgroundColor = UIColor(named: "buttonBackground")
+            albumButton.tintColor = UIColor.label
+            albumLabel.textColor = UIColor.label
+        }else{
+            albumButton.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+            albumButton.tintColor = UIColor.black
+            albumLabel.textColor = UIColor.black
+        }
 
         calcButton.layer.cornerRadius = calcButton.frame.size.width / 2
         calcButton.clipsToBounds = true
-        calcButton.backgroundColor = UIColor(named: "buttonBackground")
-        calcButton.tintColor = UIColor.label
+        if #available(iOSApplicationExtension 13.0, *) {
+            calcButton.backgroundColor = UIColor(named: "buttonBackground")
+            calcButton.tintColor = UIColor.label
+            calcLabel.textColor = UIColor.label
+        }else{
+            calcButton.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+            calcButton.tintColor = UIColor.black
+            calcLabel.textColor = UIColor.black
+        }
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
